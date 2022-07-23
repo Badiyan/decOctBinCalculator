3660af3e1b6babf393814b1687fd12e6b1df2b82
Badiyan <e.mailbox.sbv@gmail.com>
2022-07-23 15:39:11 +0300

    Add simple base value validation

--- decOctBinCalculator/Program.cs
+++ decOctBinCalculator/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace decOctBinCalculator
 {
@@ -10,17 +8,27 @@ namespace decOctBinCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input number to convert:");
+            Restart:
+            Console.WriteLine("Input number to convert, fromBase and toBase using spaces (Supported bases are 2, 8, 10 and 16): \n");
             //string number = "100";
-            string inputNumber = Console.ReadLine();
-            Console.WriteLine(inputNumber);
-            //int fromBase = 16;
-            //int toBase = 10;
+            string inputDataRaw = Console.ReadLine();
+            string[] inputDataSplitted = inputDataRaw.Split(' ');
+            
+            string[] allowedBases = { "2", "8", "10", "16" };
+            if ((allowedBases.Contains(inputDataSplitted[1]) == false) | (allowedBases.Contains(inputDataSplitted[2]) == false))  {
+                Console.WriteLine("\n Incorrect base value \n");
+                goto Restart; 
+            };
+            string numberStr = inputDataSplitted[0];
+            int fromBase = int.Parse(inputDataSplitted[1]);
+            int toBase = int.Parse(inputDataSplitted[2]);
+            string result = Convert.ToString(Convert.ToInt32(numberStr, fromBase), toBase);
+            Console.WriteLine("\n The number {0} with base{1} converted to base{2} equal {3} \n ", 
+                numberStr, fromBase, toBase, result);
+            Console.Write("Press <Enter> to next... ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
 
-            //string result = Convert.ToString(Convert.ToInt32(number, fromBase), toBase);
-
-            // result == "256"
-            // Supported bases are 2, 8, 10 and 16
+            goto Restart;
         }
     }
 }
