dc8f643637f455937af7893ad8eca8e1eb137c2b
Badiyan <e.mailbox.sbv@gmail.com>
2022-07-23 16:00:04 +0300

    add more validation

--- decOctBinCalculator/Program.cs
+++ decOctBinCalculator/Program.cs
@@ -12,8 +12,10 @@ namespace decOctBinCalculator
             Console.WriteLine("Input number to convert, fromBase and toBase using spaces (Supported bases are 2, 8, 10 and 16): \n");
             //string number = "100";
             string inputDataRaw = Console.ReadLine();
+            if (inputDataRaw == "") {
+                goto Restart;
+            }  
             string[] inputDataSplitted = inputDataRaw.Split(' ');
-            
             string[] allowedBases = { "2", "8", "10", "16" };
             if ((allowedBases.Contains(inputDataSplitted[1]) == false) | (allowedBases.Contains(inputDataSplitted[2]) == false) | (inputDataSplitted[1] == inputDataSplitted[2])) {
                 Console.WriteLine("\n Incorrect base value \n");
@@ -22,6 +24,15 @@ namespace decOctBinCalculator
             string numberStr = inputDataSplitted[0];
             int fromBase = int.Parse(inputDataSplitted[1]);
             int toBase = int.Parse(inputDataSplitted[2]);
+            try
+            {
+                string tryResult = Convert.ToString(Convert.ToInt32(numberStr, fromBase), toBase);
+            }
+            catch (FormatException) 
+            {
+                Console.WriteLine("The format of '{0}' is invalid.", numberStr);
+                goto Restart;
+            }
             string result = Convert.ToString(Convert.ToInt32(numberStr, fromBase), toBase);
             Console.WriteLine("\n The number {0} with base{1} converted to base{2} equal {3} \n ", 
                 numberStr, fromBase, toBase, result);
