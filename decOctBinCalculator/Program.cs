7c2fa36daea24907b2df0746da662dd240f6366c
Badiyan <e.mailbox.sbv@gmail.com>
2022-07-23 14:53:02 +0300

    Add project files.

---
+++ decOctBinCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace decOctBinCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number to convert:");
+            //string number = "100";
+            string inputNumber = Console.ReadLine();
+            Console.WriteLine(inputNumber);
+            //int fromBase = 16;
+            //int toBase = 10;
+
+            //string result = Convert.ToString(Convert.ToInt32(number, fromBase), toBase);
+
+            // result == "256"
+            // Supported bases are 2, 8, 10 and 16
+        }
+    }
+}
